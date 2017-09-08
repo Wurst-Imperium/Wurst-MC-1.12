c7aeb191991dd6eb6062cf6f99467ae6b84a724a
Alexander01998 <contact.wurstimperium@gmail.com>
2017-09-08 21:35:15 +0200

    Change version to 6.12.1

--- shared-src/net/wurstclient/WurstClient.java
+++ shared-src/net/wurstclient/WurstClient.java
@@ -30,7 +30,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "6.12";
+	public static final String VERSION = "6.12.1";
 	
 	public AnalyticsManager analytics;
 	public CmdManager commands;
