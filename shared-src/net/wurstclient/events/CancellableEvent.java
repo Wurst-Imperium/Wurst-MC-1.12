4519c286c9d49a35eb19e6c9256899682c461577
Alexander01998 <contact.wurstimperium@gmail.com>
2018-01-01 01:44:02 +0100

    Update copyright notices

--- shared-src/net/wurstclient/events/CancellableEvent.java
+++ shared-src/net/wurstclient/events/CancellableEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
+ * Copyright © 2014 - 2018 | Wurst-Imperium | All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
