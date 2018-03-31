6fee977da2d2f2edacec1b42434a4b967237daf5
Alexander01998 <contact.wurstimperium@gmail.com>
2018-03-31 09:04:30 +0200

    Add settings to GlideMod

--- shared-src/net/wurstclient/features/mods/movement/GlideMod.java
+++ shared-src/net/wurstclient/features/mods/movement/GlideMod.java
@@ -7,18 +7,43 @@
  */
 package net.wurstclient.features.mods.movement;
 
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.wurstclient.compatibility.WBlock;
 import net.wurstclient.compatibility.WMinecraft;
+import net.wurstclient.compatibility.WWorld;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.features.Category;
 import net.wurstclient.features.Mod;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @Mod.Bypasses(ghostMode = false, latestNCP = false, olderNCP = false)
 public final class GlideMod extends Mod implements UpdateListener
 {
+	private final SliderSetting fallSpeed = new SliderSetting("Fall speed",
+		0.125, 0.005, 0.25, 0.005, ValueDisplay.DECIMAL);
+	private final SliderSetting moveSpeed =
+		new SliderSetting("Move speed", "Horizontal movement factor.", 1.2, 1,
+			5, 0.05, ValueDisplay.PERCENTAGE);
+	private final SliderSetting minHeight = new SliderSetting("Min height",
+		"Won't glide when you are\n" + "too close to the ground.", 0, 0, 2,
+		0.01,
+		v -> v == 0 ? "disabled" : ValueDisplay.DECIMAL.getValueString(v));
+	
 	public GlideMod()
 	{
-		super("Glide", "Makes you glide down slowly instead of falling.");
+		super("Glide", "Makes you glide down slowly when falling.");
 		setCategory(Category.MOVEMENT);
+		addSetting(fallSpeed);
+		addSetting(moveSpeed);
+		addSetting(minHeight);
 	}
 	
 	@Override
@@ -36,16 +61,33 @@ public final class GlideMod extends Mod implements UpdateListener
 	@Override
 	public void onUpdate()
 	{
-		if(!WMinecraft.getPlayer().isAirBorne
-			|| WMinecraft.getPlayer().isInWater()
-			|| WMinecraft.getPlayer().isInLava()
-			|| WMinecraft.getPlayer().isOnLadder())
+		EntityPlayerSP player = WMinecraft.getPlayer();
+		
+		if(!player.isAirBorne || player.isInWater() || player.isInLava()
+			|| player.isOnLadder() || player.motionY >= 0)
 			return;
 		
-		if(WMinecraft.getPlayer().motionY >= 0)
-			return;
+		if(minHeight.getValue() > 0)
+		{
+			AxisAlignedBB box = player.getEntityBoundingBox();
+			box = box.expand(0, -minHeight.getValue(), 0);
+			if(WWorld.collidesWithAnyBlock(box))
+				return;
+			
+			BlockPos min =
+				new BlockPos(new Vec3d(box.minX, box.minY, box.minZ));
+			BlockPos max =
+				new BlockPos(new Vec3d(box.maxX, box.maxY, box.maxZ));
+			Stream<BlockPos> stream = StreamSupport
+				.stream(BlockPos.getAllInBox(min, max).spliterator(), true);
+			
+			// manual collision check, since liquids don't have bounding boxes
+			if(stream.map(WBlock::getBlock)
+				.anyMatch(b -> b instanceof BlockLiquid))
+				return;
+		}
 		
-		WMinecraft.getPlayer().motionY = -0.125F;
-		WMinecraft.getPlayer().jumpMovementFactor *= 1.21337F;
+		player.motionY = Math.max(player.motionY, -fallSpeed.getValue());
+		player.jumpMovementFactor *= moveSpeed.getValueF();
 	}
 }
---
+++ src/net/wurstclient/compatibility/WWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2014 - 2018 | Wurst-Imperium | All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package net.wurstclient.compatibility;
+
+import net.minecraft.util.math.AxisAlignedBB;
+
+public final class WWorld
+{
+	public static boolean collidesWithAnyBlock(AxisAlignedBB bb)
+	{
+		return WMinecraft.getWorld().collidesWithAnyBlock(bb);
+	}
+}
