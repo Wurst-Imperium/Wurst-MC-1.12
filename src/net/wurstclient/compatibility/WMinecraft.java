b7128b57c759a01ad1966e7649de960efdfeff9c
Alexander01998 <contact.wurstimperium@gmail.com>
2018-04-23 15:32:54 +0200

    Change display version to 1.12.2

--- src/net/wurstclient/compatibility/WMinecraft.java
+++ src/net/wurstclient/compatibility/WMinecraft.java
@@ -12,7 +12,7 @@ import net.minecraft.client.network.NetHandlerPlayClient;
 public final class WMinecraft
 {
 	public static final String VERSION = "1.12";
-	public static final String DISPLAY_VERSION = "1.12";
+	public static final String DISPLAY_VERSION = "1.12.2";
 	public static final boolean OPTIFINE = false;
 	public static final boolean REALMS = false;
 	public static final boolean COOLDOWN = true;
