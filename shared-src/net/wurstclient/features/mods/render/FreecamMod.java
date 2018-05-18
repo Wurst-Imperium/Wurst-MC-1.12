d9a03d7cbc007263a0b7fc072ffbb5987407cc13
Alexander01998 <contact.wurstimperium@gmail.com>
2018-05-18 13:42:08 +0200

    Add "Tracer" setting to Freecam

--- shared-src/net/wurstclient/features/mods/render/FreecamMod.java
+++ shared-src/net/wurstclient/features/mods/render/FreecamMod.java
@@ -7,30 +7,42 @@
  */
 package net.wurstclient.features.mods.render;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3d;
 import net.wurstclient.compatibility.WMinecraft;
+import net.wurstclient.events.CameraTransformViewBobbingListener;
 import net.wurstclient.events.PlayerMoveListener;
+import net.wurstclient.events.RenderListener;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.features.Category;
 import net.wurstclient.features.Feature;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
+import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.utils.EntityFakePlayer;
+import net.wurstclient.utils.RenderUtils;
+import net.wurstclient.utils.RotationUtils;
 
 @SearchTags({"free camera", "spectator"})
 @Mod.Bypasses
 @Mod.DontSaveState
-public final class FreecamMod extends Mod
-	implements UpdateListener, PlayerMoveListener
+public final class FreecamMod extends Mod implements UpdateListener,
+	PlayerMoveListener, CameraTransformViewBobbingListener, RenderListener
 {
 	private final SliderSetting speed =
 		new SliderSetting("Speed", 1, 0.05, 10, 0.05, ValueDisplay.DECIMAL);
+	private final CheckboxSetting tracer = new CheckboxSetting("Tracer",
+		"Draws a line to your character's actual position.", false);
 	
 	private EntityFakePlayer fakePlayer;
+	private int playerBox;
 	
 	public FreecamMod()
 	{
@@ -38,6 +50,7 @@ public final class FreecamMod extends Mod
 			"Allows you to move the camera without moving your character.");
 		setCategory(Category.RENDER);
 		addSetting(speed);
+		addSetting(tracer);
 	}
 	
 	@Override
@@ -51,6 +64,8 @@ public final class FreecamMod extends Mod
 	{
 		wurst.events.add(UpdateListener.class, this);
 		wurst.events.add(PlayerMoveListener.class, this);
+		wurst.events.add(CameraTransformViewBobbingListener.class, this);
+		wurst.events.add(RenderListener.class, this);
 		
 		fakePlayer = new EntityFakePlayer();
 		
@@ -60,6 +75,12 @@ public final class FreecamMod extends Mod
 		
 		for(KeyBinding binding : bindings)
 			binding.pressed = GameSettings.isKeyDown(binding);
+		
+		playerBox = GL11.glGenLists(1);
+		GL11.glNewList(playerBox, GL11.GL_COMPILE);
+		AxisAlignedBB bb = new AxisAlignedBB(-0.5, 0, -0.5, 0.5, 1, 0.5);
+		RenderUtils.drawOutlinedBox(bb);
+		GL11.glEndList();
 	}
 	
 	@Override
@@ -67,6 +88,8 @@ public final class FreecamMod extends Mod
 	{
 		wurst.events.remove(UpdateListener.class, this);
 		wurst.events.remove(PlayerMoveListener.class, this);
+		wurst.events.remove(CameraTransformViewBobbingListener.class, this);
+		wurst.events.remove(RenderListener.class, this);
 		
 		fakePlayer.resetPlayerPosition();
 		fakePlayer.despawn();
@@ -77,6 +100,9 @@ public final class FreecamMod extends Mod
 		player.motionZ = 0;
 		
 		mc.renderGlobal.loadRenderers();
+		
+		GL11.glDeleteLists(playerBox, 1);
+		playerBox = 0;
 	}
 	
 	@Override
@@ -101,5 +127,64 @@ public final class FreecamMod extends Mod
 	public void onPlayerMove(EntityPlayerSP player)
 	{
 		player.noClip = true;
+	}
+	
+	@Override
+	public void onCameraTransformViewBobbing(
+		CameraTransformViewBobbingEvent event)
+	{
+		if(tracer.isChecked())
+			event.cancel();
+	}
+	
+	@Override
+	public void onRender(float partialTicks)
+	{
+		if(fakePlayer == null || !tracer.isChecked())
+			return;
+		
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		GL11.glColor4f(1, 1, 1, 0.5F);
+		
+		// box
+		GL11.glPushMatrix();
+		GL11.glTranslated(fakePlayer.posX, fakePlayer.posY, fakePlayer.posZ);
+		GL11.glScaled(fakePlayer.width + 0.1, fakePlayer.height + 0.1,
+			fakePlayer.width + 0.1);
+		GL11.glCallList(playerBox);
+		GL11.glPopMatrix();
+		
+		// line
+		Vec3d start = RotationUtils.getClientLookVec()
+			.addVector(0, WMinecraft.getPlayer().getEyeHeight(), 0)
+			.addVector(mc.getRenderManager().renderPosX,
+				mc.getRenderManager().renderPosY,
+				mc.getRenderManager().renderPosZ);
+		Vec3d end = fakePlayer.getEntityBoundingBox().getCenter();
+		
+		GL11.glBegin(GL11.GL_LINES);
+		GL11.glVertex3d(start.xCoord, start.yCoord, start.zCoord);
+		GL11.glVertex3d(end.xCoord, end.yCoord, end.zCoord);
+		GL11.glEnd();
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
